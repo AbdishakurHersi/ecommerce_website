ac8017d81bbb5ea211264af45bc6a2452a59e217
Abdi Hersi <abdi_shakur23@hotmail.com>
2023-04-27 15:48:29 -0600

    created cateogry model

--- backend/app/app.js
+++ backend/app/app.js
@@ -8,6 +8,9 @@ import {
 } from "../middlewares/globalErrorHandler.js";
 import userRoutes from "../routes/usersRoute.js";
 import productRoutes from "../routes/productRoute.js";
+import categoriesRouter from "../routes/categoriesRouter.js";
+import colorRouter from "../routes/colorRouter.js";
+import brandRouter from "../routes/brandRouter.js";
 
 dbConnect();
 const app = express();
@@ -15,6 +18,9 @@ app.use(express.json());
 //route
 app.use("/api/v1/users/", userRoutes);
 app.use("/api/v1/products/", productRoutes);
+app.use("/api/v1/categories/", categoriesRouter);
+app.use("/api/v1/brands/", brandRouter);
+app.use("/api/v1/color/", colorRouter);
 app.use(notFound);
 app.use(globalErrorHandler);
 
