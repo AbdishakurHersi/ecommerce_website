cfa901c5930c7014ed13ebf5d52a2d5ec27999d3
Abdi Hersi <abdi_shakur23@hotmail.com>
2023-04-28 14:40:31 -0600

    created review model

---
+++ backend/routes/reviewRouter.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { createReviewCtrl } from "../controllers/reviewController.js";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+const reviewRouter = express.Router();
+reviewRouter.post("/:productID", isLoggedIn, createReviewCtrl);
+export default reviewRouter;
